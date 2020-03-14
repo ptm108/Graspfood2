cb93636f18b8b883328e776fbf862627d0414f52
TM ptm108 <tzemingg96@gmail.com>
2020-03-14 16:36:47 +0800

    added form reducer, fixed form input

--- client/src/app/reduxstore/configureStore.js
+++ client/src/app/reduxstore/configureStore.js
@@ -1,10 +1,12 @@
 import { createStore, combineReducers } from "redux";
 import testReducer from "../components/test/testReducer";
 import authReducer from "../components/auth/authReducer";
+import { reducer as FormReducer } from "redux-form";
 
 const rootReducer = combineReducers({
     test: testReducer,
-    auth: authReducer
+    auth: authReducer,
+    form: FormReducer
 })
 
 export const configureStore = () => {
