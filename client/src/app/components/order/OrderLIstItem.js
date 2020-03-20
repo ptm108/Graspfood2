4bccb4a67fe7bf04253105382b2b3c71e3d1ae78
crslegend <risheng17@gmail.com>
2020-03-20 15:45:26 +0800

    restructure files for rider, order, review

---
+++ client/src/app/components/order/OrderLIstItem.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const OrderLIstItem = () => {
+  return <div></div>;
+};
+
+export default OrderLIstItem;
---
+++ client/src/app/components/review/ReviewList.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class ReviewList extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default ReviewList;
