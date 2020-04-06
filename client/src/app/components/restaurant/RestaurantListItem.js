05c51362ddca7d0b92156ea4c50a4fa7ca55e3d0
TM ptm108 <tzemingg96@gmail.com>
2020-04-06 10:15:08 +0800

    added random images to customer dashboard

--- client/src/app/components/restaurant/RestaurantListItem.js
+++ client/src/app/components/restaurant/RestaurantListItem.js
@@ -1,11 +1,14 @@
 import React, { Component, Fragment } from "react";
-import { Card } from "semantic-ui-react";
+import { Card, Image } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 
 const RestaurantListItem = ({ restaurant }) => {
-  console.log(restaurant);
+  // console.log(restaurant);
+  var imagesrc = "/assets/" + Math.floor(Math.random() * Math.floor(7)) + ".jpg";
+  // console.log(imagesrc);
   return (
     <Card centered color="teal" as={Link} to={`/restaurant/${restaurant.rid}`}>
+    <Image src={imagesrc} wrapped ui={false} />
       <Card.Content>
         <Card.Header>{restaurant.rname}</Card.Header>
         <Card.Meta>{restaurant.streetname}</Card.Meta>
