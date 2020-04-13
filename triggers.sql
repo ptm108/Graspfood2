09cdd3981979830897b255f93db157b9c513942a
eliz <e0319168@u.nus.edu>
2020-04-13 19:05:45 +0800

    updated  the triggers

--- triggers.sql
+++ triggers.sql
@@ -30,6 +30,23 @@ Create trigger update_rider_state_trigger
 Execute procedure Update_rider_state();
 
 
+Create or replace function set_rider_state()
+Returns trigger as $$
+    Begin
+        Update deliveryrider set isidle = true where uid = NEW.uid and NEW.riderdelivertime is not null
+;
+    Return null;
+End;
+$$ LANGUAGE plpgsql ;
+
+Drop trigger if exists set_rider_state_trigger on delivers;
+Create trigger set_rider_state_trigger
+    After update of riderdelivertime
+    on delivers
+    For each row
+Execute procedure set_rider_state();
+
+
 Create or replace function update_reward_points()
 Returns trigger as $$
     Begin
@@ -49,38 +66,38 @@ Execute procedure update_reward_points();
 Create or replace function update_delivery_rider_rating()
 Returns trigger as $$
     Begin
-        Update deliveryrider set deliveryriderrating = (deliveryriderrating + NEW.deliveryservicerating) / 2 where uid = NEW.uid;
+        Update deliveryrider set deliveryriderrating = (select avg(deliveryriderrating) from delivers) where uid = NEW.uid;
     Return null;
 End;
 $$ LANGUAGE plpgsql ;
 
 Drop trigger if exists update_delivery_rider_rating_trigger on delivers;
 Create trigger update_delivery_rider_rating_trigger
-    After update
+    After update of deliveryservicerating
     on delivers
     For each row
 Execute procedure update_delivery_rider_rating();
 
 
-Create or replace function set_delivery_rider_rating()
-Returns trigger as $$
-    Begin
-        Update deliveryrider set deliveryriderrating = 5 where uid = NEW.uid;
-    Return null;
-End;
-$$ LANGUAGE plpgsql ;
+-- Create or replace function set_delivery_rider_rating()
+-- Returns trigger as $$
+--     Begin
+--         Update deliveryrider set deliveryriderrating = 5 where uid = NEW.uid;
+--     Return null;
+-- End;
+-- $$ LANGUAGE plpgsql ;
 
-Drop trigger if exists set_delivery_rider_rating_trigger on deliveryrider;
-Create trigger set_delivery_rider_rating_trigger
-    After insert
-    on deliveryrider
-    For each row
-Execute procedure set_delivery_rider_rating();
+-- Drop trigger if exists set_delivery_rider_rating_trigger on deliveryrider;
+-- Create trigger set_delivery_rider_rating_trigger
+--     After insert
+--     on deliveryrider
+--     For each row
+-- Execute procedure set_delivery_rider_rating();
 
 Create or replace function update_restaurant_rating()
 Returns trigger as $$
     Begin
-        Update restaurant set rating = (rating + NEW.rating) / 2 where rid = NEW.rid;
+        Update restaurant set rating = (select avg(rating) from reviews) where rid = NEW.rid;
     Return null;
 End;
 $$ LANGUAGE plpgsql ;
@@ -94,7 +111,7 @@ Execute procedure update_restaurant_rating();
 
 Drop trigger if exists update_restaurant_rating_trigger_update on reviews;
 Create trigger update_restaurant_rating_trigger_update
-    After update
+    After update of rating
     on reviews
     For each row
 Execute procedure update_restaurant_rating();
